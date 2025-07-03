b0fbf1153092314c7241f55048a0a459e99a325a
blue870512 <guhl_2009@163.com>
2025-07-03 14:39:06 +0800

    remove useless code

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,7 +1,5 @@
-mod parser;
 pub mod types;
 pub mod manager;
 
-pub use parser::XmlConfigParser;
 pub use types::{BizExtension, FieldDef, MessageDef, MessageConfig};
 pub use manager::ConfigManager;
